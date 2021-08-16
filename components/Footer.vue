da0e32a8c5ad61e801b3de6b0363888bcabb4a4d
Marko Dumnic <markodumnic8@gmail.com>
2021-08-16 16:49:52 +0200

    Adding all

---
+++ components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer>
+        This is place where u can put copyright props...
+    </footer>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="content">
+    <AppMenu />
+    <Nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import AppMenu from "@/components/AppMenu.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  components: {
+    AppMenu,
+    Footer,
+  },
+};
+</script>
+
+
+<style>
+html {
+  font-family: "Mulish", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+h1,
+h2,
+h3,
+h4 {
+  font-family: "Poppins", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-weight: 600;
+}
+</style>
